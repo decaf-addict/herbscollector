5a1f5e40ffa1de54cbb088150f6a3638f31f99a0
Tonkers <tonkers.society@gmail.com>
2022-02-23 15:50:35 -0800

    from explorer

--- HerbsCollector.py
+++ HerbsCollector.py
@@ -3,12 +3,12 @@ from brownie import Contract, accounts
 
 
 def main():
-    dev = accounts.add(os.getenv("PRIVATE_KEY"))
+    dev = accounts.add(os.getenv("OWNER_KEY"))
     bb_usdc = "0x3B998BA87b11a1c5BC1770dE9793B17A0dA61561"
     bb_dai = "0x2Ff1552Dd09f87d6774229Ee5eca60CF570AE291"
-    herbs = Contract('0x261e122e2c6aa341B9f132D37C621457d797Bb47')
-    dai = Contract("0x8D11eC38a3EB5E956B052f67Da8Bdc9bef8Abf3E")
-    usdc = Contract("0x04068DA6C83AFCFA0e13ba15A6696662335D5B75")
+    herbs = Contract.from_explorer('0x261e122e2c6aa341B9f132D37C621457d797Bb47')
+    dai = Contract.from_explorer("0x8D11eC38a3EB5E956B052f67Da8Bdc9bef8Abf3E")
+    usdc = Contract.from_explorer("0x04068DA6C83AFCFA0e13ba15A6696662335D5B75")
     usdc_herbs = herbs.sample(bb_usdc, {"from": dev})
     dai_herbs = herbs.sample(bb_dai, {"from": dev})
     if usdc_herbs[0] > 3000 * 1e6 or dai_herbs[0] > 3000 * 1e18:
